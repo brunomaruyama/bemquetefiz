4dab5ef867cfb474808e9897bac80e5e5ae64562
brunomaruyama <brunoyuitiro@hotmail.com>
2024-08-19 04:07:46 -0300

    layout, burger menu, card

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,36 @@
+import Header from "@/components/header/page";
 import Card from "../components/card/page";
 import Image from "next/image";
+import WhatsButton from "@/components/whatsbutton/whatsbutton";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div id="home" className=" w-full h-[33vh]">
-        <h1 className="text-3xl text-center">Bem Que te Fiz</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi tempora
-          voluptates possimus qui nobis voluptatem officia velit. Atque fugit
-          aliquid incidunt omnis, excepturi mollitia sint, ipsum dicta ex
-          molestias cumque!
-        </p>
+      <Header />
+      <div
+        id="home"
+        className="w-full h-[40vh] flex flex-col justify-center items-center p-4 overflow-clip"
+      >
+        <Image
+          src="/logo.jpg"
+          width={400}
+          height={100}
+          alt="logo"
+          className="mix-blend-multiply"
+        />
+        <div className="w-4/5 sm:w-1/2 rounded-3xl relative bottom-0 bg-white/70 p-5">
+          <p className="font-extralight">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
+            tempora voluptates possimus qui nobis voluptatem officia velit.
+            Atque fugit aliquid incidunt omnis, excepturi mollitia sint, ipsum
+            dicta ex molestias cumque!
+          </p>
+        </div>
       </div>
       <div className="p-4">
         <Card />
       </div>
+      <WhatsButton />
     </main>
   );
 }
---
+++ components/header/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import "../../app/globals.css";
+
+export default function Header() {
+  const sections = ["INÍCIO", "PRODUTOS", "SOBRE", "CONTATO"];
+  const listSection = sections.map((section) => (
+    <li
+      className="hover:text-rose-400 hover:translate-y-1 duration-300 font-extralight"
+      key={1}
+    >
+      <a href={`#${section}`}>{section}</a>
+    </li>
+  ));
+
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <header className="w-full flex justify-between items-center px-[5vw] py-3">
+      <Image
+        src="/logo.jpg"
+        alt="logo bem que te fiz"
+        width={130}
+        height={40}
+      />
+      <button
+        className="absolute top-1 rounded-full right-5 sm:hidden h-12 w-12 bg-gray-300 grid place-items-center p-2"
+        onClick={() => setOpenMenu((prev) => !prev)}
+      >
+        <div
+          id={openMenu ? "topbar" : ""}
+          className="bg-gray-800  h-[1px] w-5 rounded-full transition-all duration-150"
+        ></div>
+        <div
+          id={openMenu ? "hidebar" : ""}
+          className="bg-gray-800  h-[1px] w-5 rounded-full transition-all duration-150"
+        ></div>
+        <div
+          id={openMenu ? "bottombar" : ""}
+          className="bg-gray-800  h-[1px] w-5 rounded-full transition-all duration-150"
+        ></div>
+      </button>
+      <div
+        id={openMenu ? "openmenu" : "closemenu"}
+        className="sm:contents clear-both"
+      >
+        <ul className="mt-1 mr-12 sm:flex sm:space-x-3">{listSection}</ul>
+      </div>
+    </header>
+  );
+}
