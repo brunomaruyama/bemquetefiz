8bcc11f6cf36cffd85590a35dc3617276fffb87b
brunomaruyama <brunoyuitiro@hotmail.com>
2024-08-22 21:29:24 -0300

    card proportion and toggle

--- components/card/page.tsx
+++ components/card/page.tsx
@@ -1,42 +1,68 @@
+"use client";
+import { useState } from "react";
 import { produtos } from "../produtos/produtos";
 import Image from "next/image";
 
 export default function Card() {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
   return produtos.map((produto) => {
     return (
-      <div
+      <label
+        htmlFor={produto.nome}
         key={produto.nome}
-        className="overflow-hidden shadow-lg group hover:shadow-rose-200 duration-500 max-w-[80vw] sm:max-w-[240px] rounded-3xl"
+        className="cursor-pointer"
       >
-        <Image
-          className="w-full opacity-85 group-hover:opacity-100 group-hover:scale-105 duration-300 overflow-hidden"
-          src={produto.src}
-          alt={produto.nome}
-          width={320}
-          height={320}
+        <input
+          type="checkbox"
+          className="sr-only "
+          id={produto.nome}
+          checked={isChecked}
+          onChange={handleCheckboxChange}
         />
-        <div className="p-2 space-y-2 text-sm leading-tight">
-          <h3 className="font-bold ">{produto.nome}</h3>
-          <p className="font-extralight overflow-hidden ">
-            {produto.descricao}
-          </p>
-          <p className="font-semibold">
-            Ingredientes:{" "}
-            <span className="font-extralight overflow-hidden">
-              {produto.ingredientes.join(", ")}
-            </span>
-          </p>
-          <p className="font-extralight">
-            <span className="font-semibold">
-              {produto.peso ? "Peso: " : "Conteudo: "}{" "}
-            </span>
-            {produto.peso ? produto.peso : produto.conteudo}
-          </p>
-          <p className="text-center font-extralight">
-            R$ <span className="font-bold text-base">{produto.preco}</span>
-          </p>
+        <div className="overflow-hidden shadow-lg hover:shadow-rose-200 duration-500 max-w-[80vw] sm:max-w-[240px] rounded-3xl group flex flex-col h-full">
+          <div className="h-[260px] mb-3">
+            <Image
+              className={`h-full w-full opacity-85 group-hover:opacity-100 group-hover:scale-105 duration-300 overflow-hidden object-cover`}
+              src={produto.src}
+              alt={produto.nome}
+              width={320}
+              height={320}
+            />
+          </div>
+          <div className="p-2 space-y-2 text-sm leading-tight text-left">
+            <h3 className="font-bold ">{produto.nome}</h3>
+            <p
+              className={`${
+                isChecked ? "line-clamp-none" : "line-clamp-4"
+              } font-extralight line-clamp duration-300`}
+            >
+              {produto.descricao}
+            </p>
+            <p className="font-semibold">
+              Ingredientes:{" "}
+              <span
+                className={`${
+                  isChecked ? "line-clamp-none" : "line-clamp-3"
+                } font-extralight line-clamp duration-300`}
+              >
+                {produto.ingredientes.join(", ")}
+              </span>
+            </p>
+            <p className="font-extralight">
+              <span className="font-semibold">
+                {produto.peso ? "Peso: " : "Conteudo: "}{" "}
+              </span>
+              {produto.peso ? produto.peso : produto.conteudo}
+            </p>
+            <p className="text-center font-extralight">
+              R$ <span className="font-bold text-base">{produto.preco}</span>
+            </p>
+          </div>
         </div>
-      </div>
+      </label>
     );
   });
 }
