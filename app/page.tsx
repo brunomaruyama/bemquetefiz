b532158adab9fd8376b041f8b864e36ebe0dd880
brunomaruyama <brunoyuitiro@hotmail.com>
2025-01-09 19:48:29 -0300

    merge

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Card from "../components/card/page";
 import Image from "next/image";
 import WhatsButton from "@/components/whatsbutton/whatsbutton";
 import Footer from "@/components/footer/page";
-import { ProductProps } from "@/types/products";
 import { produtos } from "@/components/produtos/produtos";
 
 export default function Home() {
@@ -12,7 +11,7 @@ export default function Home() {
       <Header />
       <div
         id="home"
-        className="w-full h-3/4 sm:h-1/2 flex flex-col justify-center items-center p-4"
+        className="w-full h-3/4 sm:h-1/2 flex flex-col justify-center items-center p-4 mb-2"
       >
         <div className="flex flex-shrink object-scale-down">
           <Image
@@ -23,8 +22,8 @@ export default function Home() {
             className="mix-blend-multiply h-auto w-auto object-contain"
           />
         </div>
-        <div className="w-4/5 lg:w-1/2 rounded-3xl bg-white/75 p-5">
-          <p className="font-extralight text-sm leading-snug">
+        <div className="w-4/5 lg:w-1/3 rounded-3xl bg-white/75 p-6 relative shadow-lg z-10 my-4">
+          <p className="font-extralight text-md leading-relaxed text-center">
             “Sabonetes, hidratantes, shampoos e condicionadores…e sempre algo
             mais! Aqui você encontra seus produtos para cuidados diários da pele
             e dos cabelos, formulados com ingredientes provenientes da natureza
